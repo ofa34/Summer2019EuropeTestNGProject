90f56ea659c0807c12ea2edbec9cb8b1e9d98140
Omer Faruk Ayguner <omerayguner@hotmail.com>
2019-11-03 23:29:37 +0300

    day3 test cases

---
+++ src/test/java/com/cybertek/tests/day3_webelement_intro/VerifyURLchanged.java
@@ -0,0 +1,44 @@
+package com.cybertek.tests.day3_webelement_intro;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class VerifyURLchanged {
+    /**
+     * open browser
+     * go to http://practice.cybertekschool.com/forgot_password Links to an external site.
+     * enter any email
+     * click on Retrieve password
+     * verify that url changed to http://practice.cybertekschool.com/email_sent
+     */
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+
+        //enter any email
+        WebElement emailInput = driver.findElement(By.name("email"));
+
+        //sendKeys() --> send keyboard actions to webelement / enters given text
+        emailInput.sendKeys("email@gmail.com");
+
+        //
+        WebElement retrievePasswordButton = driver.findElement(By.id("form_submit"));
+        retrievePasswordButton.click();
+
+        String expectedUrl = "http://practice.cybertekschool.com/email_sent";
+
+        String actualUrl = driver.getCurrentUrl();
+        if(expectedUrl.equals(actualUrl)){
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAıL");
+            System.out.println("actualUrl = "+actualUrl);
+            System.out.println("expectedUrl = "+expectedUrl);
+        }
+
+        driver.close();
+
+    }
+}
